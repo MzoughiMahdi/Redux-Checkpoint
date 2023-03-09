d4c295a95e51e6e4de01be8c02d3bf85aec54fac
MzoughiMahdi <mahdi.mzoughi@esprit.tn>
2023-03-09 12:18:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import TodoInputs from "./components/TodoInputs";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App m-5">
+      <h1>TODO LIST</h1>
+      <TodoInputs />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
